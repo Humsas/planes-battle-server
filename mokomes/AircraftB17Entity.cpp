e4b7ecfa155ada681fc8c6d324e123e55910a886
Humsas <bekertas@gmail.com>
2013-05-23 22:12:26 +0300

    kolizijos ir bugai

---
+++ mokomes/AircraftB17Entity.cpp
@@ -0,0 +1,379 @@
+#include "AircraftB17Entity.h"
+
+#include "console.h"
+extern Console gCons;
+
+
+//AircraftB17::AircraftB17(ColisionManager *colisionManager, Vector initPos)
+//{
+//	this->initPos = initPos;
+//
+//	this->colisionManager = colisionManager;	
+//	op = new objectPosition(Vector(0, 0, 0), 2);
+//
+//
+//	FDMExec = new JSBSim::FGFDMExec();
+//	FDMExec->SetDebugLevel(0);
+//	FDMExec->SetOutputFileName("");
+//	FDMExec->SetAircraftPath("aircraft");
+//	FDMExec->SetEnginePath("engine");
+//	FDMExec->SetSystemsPath("systems");
+//
+//	//FDMExec->LoadModel("B17");
+//
+//	//FDMExec->GetPropulsion()->InitRunning(-1);
+//
+//	FDMExec->LoadScript("scripts\\testScript");
+//	FDMExec->GetPropulsion()->InitRunning(-1);
+//	FDMExec->GetIC()->SetTerrainElevationFtIC(initPos.z/0.3048);
+//	/*FDMExec->GetIC()->SetUBodyFpsIC(0.0);
+//	FDMExec->GetIC()->SetVBodyFpsIC(0.0);
+//	FDMExec->GetIC()->SetWBodyFpsIC(0.0);
+//
+//	FDMExec->GetIC()->SetLatitudeDegIC(0);
+//	FDMExec->GetIC()->SetLongitudeDegIC(0);
+//
+//	FDMExec->GetIC()->SetPhiDegIC(0.0);
+//	FDMExec->GetIC()->SetThetaDegIC(0.0);
+//	FDMExec->GetIC()->SetPsiDegIC(12.0);
+//
+//	FDMExec->GetIC()->SetAltitudeAGLFtIC(18000);
+//	*/
+//
+//	/*FDMExec->GetAtmosphere()->SetExTemperature(9.0/5.0*(1+273.15) );
+//	FDMExec->GetAtmosphere()->SetExDensity(1);
+//	FDMExec->GetAtmosphere()->SetExPressure(200);*/
+//	/*FDMExec->GetPropagate()->SetSeaLevelRadius(0);
+//	FDMExec->GetAtmosphere()->SetWindNED(1,0,0);
+//	FDMExec->GetAtmosphere()->SetWindspeed(10);*/
+//
+//	//FDMExec->GetIC()->Load("reset00");
+//	//FDMExec->RunIC();
+//
+//	FDMExec->DoTrim(JSBSim::tGround);
+//
+//	//startEngine();
+//
+//	//FDMExec->GetPropagate()->GetLocation().
+//
+//	// initial settings
+//	throttle	= 0.0;
+//	ailerons	= 0.0;
+//	elevator	= 0.0;
+//	ruder		= 0.0;
+//	flaps		= 0.0;
+//
+//	propAdvance = 0.0;
+//
+//	starter		= 0;
+//	magnetos	= 0;
+//
+//	mixture		= 1.0;
+//
+//	speed		= 0.0;
+//
+//	brakes		= 0.0;
+//	gearDown	= 1;
+//
+//	stallWarn	= 0.0;
+//
+//	position.x = initPos.x;
+//	position.y = initPos.y;
+//	position.z = initPos.z;
+//
+//	copyToJSBSim();
+//	FDMExec->RunIC();
+//	copyFromJSBSim();
+//
+//}
+
+AircraftB17::~AircraftB17()
+{
+	delete FDMExec;
+}
+
+void AircraftB17::reset()
+{
+	FDMExec->GetIC()->SetTerrainElevationFtIC(initPos.z/0.3048);
+
+
+	FDMExec->GetIC()->SetPhiDegIC(-initRotation.z);
+	FDMExec->GetIC()->SetThetaDegIC(initRotation.y);
+	FDMExec->GetIC()->SetPsiDegIC(-(initRotation.x+90));
+
+	/*FDMExec->GetIC()->SetAltitudeAGLFtIC(18000);*/
+
+
+	/*FDMExec->GetAtmosphere()->SetExTemperature(9.0/5.0*(1+273.15) );
+	FDMExec->GetAtmosphere()->SetExDensity(1);
+	FDMExec->GetAtmosphere()->SetExPressure(200);*/
+	/*FDMExec->GetPropagate()->SetSeaLevelRadius(0);
+	FDMExec->GetAtmosphere()->SetWindNED(1,0,0);
+	FDMExec->GetAtmosphere()->SetWindspeed(10);*/
+
+	//FDMExec->GetIC()->Load("reset00");
+	//FDMExec->RunIC();
+
+	FDMExec->DoTrim(JSBSim::tGround);
+
+	//startEngine();
+
+	//FDMExec->GetPropagate()->GetLocation().
+
+	// initial settings
+	throttle	= 0.0;
+	ailerons	= 0.0;
+	elevator	= 0.0;
+	ruder		= 0.0;
+	flaps		= 0.0;
+	propAdvance = 0.0;
+	starter		= 0;
+	magnetos	= 0;
+	mixture		= 1.0;
+	speed		= 0.0;
+	brakes		= 0.0;
+	gearDown	= 1;
+	stallWarn	= 0.0;
+
+	this->position.x = initPos.x;
+	this->position.y = initPos.y;
+	this->position.z = initPos.z;
+
+	copyToJSBSim();
+	FDMExec->RunIC();
+	copyFromJSBSim();
+}
+
+void AircraftB17::startEngine(bool starter)
+{
+	magnetos = 3;
+	this->starter = (starter)?  1 :  0;
+}
+
+void AircraftB17::stopEngine()
+{
+	magnetos = 0;
+}
+
+void AircraftB17::Update(float dt)
+{
+	if(!isClientStub)
+	{
+		mProjectileBombCooldown.UpdateCooldowns(dt);
+
+		setElevation(CM->getMapHeightAtPoint(position));
+
+		FDMExec->GetState()->Setdt(dt);
+		copyToJSBSim();
+		// info ivedimas
+
+
+		FDMExec->Run();
+
+		// info istraukimas
+
+		copyFromJSBSim();
+
+		/*op->pozicija.y = FDMExec->GetPropagate()->GetLatitudeDeg() * 111000.0f + initPos.y;
+		op->pozicija.x = FDMExec->GetPropagate()->GetLongitudeDeg() * 111000.0f + initPos.x;
+		op->pozicija.z = FDMExec->GetPropagate()->GetAltitudeASLmeters();*/
+
+
+		position.x = FDMExec->GetPropagate()->GetLatitudeDeg() * 111000.0f + initPos.x;
+		position.y = FDMExec->GetPropagate()->GetLongitudeDeg() * 111000.0f + initPos.y;
+		position.z = FDMExec->GetPropagate()->GetAltitudeASLmeters();
+
+
+
+		rotarionYawPitchRoll.x = -RadiansToDegrees(FDMExec->GetPropagate()->GetEuler(3))-90;
+		rotarionYawPitchRoll.y = RadiansToDegrees(FDMExec->GetPropagate()->GetEuler(2));
+		rotarionYawPitchRoll.z = -RadiansToDegrees(FDMExec->GetPropagate()->GetEuler(1));
+
+
+		FGPiston* eng = (FGPiston*)FDMExec->GetPropulsion()->GetEngine(0);
+
+
+
+		//stringstream ss1;//create a stringstream
+		//stringstream ss2;
+		//stringstream ss3;
+		//stringstream ss4;
+		//stringstream ss5;
+		//stringstream ss6;
+		//stringstream ss7;
+		//stringstream ss8;
+		//
+		//ss1 << "Runing: " << eng->GetRunning();
+		//ss2 << "RPM: " << eng->getRPM();
+		//ss3 << "Starter: " << eng->GetStarter();
+		//ss4 << "Starved: " << eng->GetStarved();
+		//ss5 << "Throttle: " << eng->GetThrottle();
+		//ss6 << "Mixture: " << eng->GetMixture();
+		//ss7 << "Flaps: " << flaps;
+		//ss8 << "Prop Pitch: " << propAdvance;
+
+
+		/*gCons.add(ss1.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss2.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss3.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss4.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss5.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss6.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss7.str(),GAME_CONSOLE_WARNING);
+		gCons.add(ss8.str(),GAME_CONSOLE_WARNING);*/
+	}
+	rotateYPR(&rotarionYawPitchRoll);
+	combine();
+}
+
+
+void AircraftB17::setElerons(float pos)
+{
+	if(-1.0 <= pos && pos <= 1.0)
+		ailerons = pos;
+}
+
+void AircraftB17::setElevator(float pos)
+{
+	if(-1.0 <= pos && pos <= 1.0)
+		elevator = pos;
+}
+
+void AircraftB17::setRuder(float pos)
+{
+	if(-1.0 <= pos && pos <= 1.0)
+		ruder = pos;
+}
+
+void AircraftB17::setFlaps(float pos)
+{
+	if(-1.0 <= pos && pos <= 1.0)
+		flaps = pos;
+}
+
+
+void AircraftB17::setThrottle(float pos)
+{
+	throttle = pos;
+	if(throttle > 1.0) throttle = 1.0;
+	if(throttle < 0.00000001) throttle = 0.0;
+}
+
+void AircraftB17::increaseThrottle()
+{
+	if(throttle < 1.0) throttle += 0.1;
+	if(throttle >= 1.0) throttle = 1.0;
+}
+
+void AircraftB17::decreaseThrottle()
+{
+	if(throttle > 0.0) throttle -= 0.1;
+	if(throttle <= 0.00000001) throttle = 0.0;
+}
+
+void AircraftB17::increaseMixture()
+{
+	if(mixture < 1.0) mixture += 0.05;
+	if(mixture >= 1.0) mixture = 1.0;
+}
+
+
+void AircraftB17::decreaseMixture()
+{
+	if(mixture > 0.0) mixture -= 0.05;
+	if(mixture <= 0.00000001) mixture = 0.0;
+}
+
+
+void AircraftB17::increasePropAdvance()
+{
+	if(propAdvance < 1.0) propAdvance += 0.05;
+	if(propAdvance >= 1.0) propAdvance = 1.0;
+}
+
+
+void AircraftB17::decreasePropAdvance()
+{
+	if(propAdvance > 0.0) propAdvance -= 0.05;
+	if(propAdvance <= 0.00000001) propAdvance = 0.0;
+}
+
+void AircraftB17::copyToJSBSim()
+{
+	FDMExec->GetFCS()->SetDaCmd(ailerons);
+	FDMExec->GetFCS()->SetDeCmd(elevator);
+	FDMExec->GetFCS()->SetDrCmd(ruder);
+	FDMExec->GetFCS()->SetDfCmd(flaps);
+
+	FDMExec->GetFCS()->SetCBrake(brakes);
+	FDMExec->GetFCS()->SetLBrake(brakes);
+	FDMExec->GetFCS()->SetRBrake(brakes);
+
+	FDMExec->GetFCS()->SetDsCmd(ruder);
+
+	FDMExec->GetFCS()->SetGearCmd(gearDown);
+
+
+	for(int i = 0; i < FDMExec->GetPropulsion()->GetNumEngines(); i++)
+	{
+		FDMExec->GetFCS()->SetThrottleCmd(i, throttle);
+		FDMExec->GetFCS()->SetMixtureCmd(i, mixture);
+
+		FDMExec->GetFCS()->SetPropAdvance(i, propAdvance);
+
+	}
+
+	FDMExec->GetPropulsion()->SetMagnetos( magnetos );
+	FDMExec->GetPropulsion()->SetStarter( starter );
+
+
+	//FDMExec->GetGroundReactions()->GetGearUnit(1)->
+}
+
+
+void AircraftB17::copyFromJSBSim()
+{
+	Vector spd = Vector(
+		FDMExec->GetPropagate()->GetVel(1),
+		FDMExec->GetPropagate()->GetVel(2),
+		FDMExec->GetPropagate()->GetVel(3));
+
+
+	speed = spd.Magnitude() * 1.09728; // ft/s to km/h
+
+	stallWarn = FDMExec->GetAerodynamics()->GetStallWarn();
+
+	/*stringstream ss1;
+	ss1 << "STALL: " << FDMExec->GetAerodynamics()->GetStallWarn(); 
+	gCons.add(ss1.str(),GAME_CONSOLE_WARNING);*/
+}
+
+
+void AircraftB17::setBrakes(bool b)
+{
+	if(b)
+		brakes = 0.3;
+	else
+		brakes = 0.0;
+}
+
+void AircraftB17::gearUpDown()
+{
+	gearDown = (gearDown == 1)? 0 : 1;
+}
+
+
+float AircraftB17::getSpeed()
+{
+	return speed;
+}
+
+bool AircraftB17::getStall()
+{
+	return (stallWarn > 0.001)? true : false;
+}
+
+void AircraftB17::setElevation(double elev)
+{
+	FDMExec->GetPropagate()->SetTerrainElevation(elev/0.3048);
+}
---
+++ mokomes/sceneManager.cpp
@@ -0,0 +1,267 @@
+#include "sceneManager.h"
+
+#include "console.h"
+extern Console gServerConsole;
+
+//konstruktorius
+SceneManager::SceneManager(LPDIRECT3DDEVICE9 &d3)
+{
+	this->d3 = d3;
+
+	
+	textureManager	= new TextureManager(d3);
+	meshManager		= new Mesh(d3, textureManager);
+
+	kamera			= new Kamera();
+	CM				= new ChunkManager(d3, textureManager, kamera);
+
+	//ter				= new Terrain(d3, textureManager, kamera, "Maps/Map0/map.BMP");
+	
+	
+	sceneObjects	= new SceneObjects(d3, meshManager);
+	hud				= new Hud(d3, textureManager);
+	debesis = new Clouds(d3);
+	slopeScale = 0.0;
+	depthBias = 0.0;
+
+	wireframe = false;
+	//renderConsole = false;
+
+	LoadObjects();
+
+	//ter->generateTerain();
+	//ter->Update();
+	//ter->update(6000, 6000, 0, 0);
+}
+
+//destruktorius
+SceneManager::~SceneManager()
+{
+	delete CM;
+	//if(ter != NULL)				delete ter;
+	if(kamera != NULL)			delete kamera;
+	if(meshManager != NULL)		delete meshManager;
+	if(sceneObjects != NULL)	delete sceneObjects;
+	if(hud != NULL)				delete hud;
+	delete						textureManager;
+
+}
+
+void SceneManager::Render()
+{
+	// cia uzkrauti kalna ivairiu directX parametru renderinimui
+	d3->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(250, 210, 180), 1.0f, 0);
+	d3->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+
+	d3->BeginScene();
+
+	//D3DXMATRIX matView;    // the view transform matrix
+
+	//D3DXMatrixLookAtLH(&matView,
+	//	&D3DXVECTOR3 (kamera->getCamP().x, kamera->getCamP().y, kamera->getCamP().z),    // the camera position
+	//	&D3DXVECTOR3 (kamera->getCamW().x, kamera->getCamW().y, kamera->getCamW().z),	// the look-at position
+	//	&D3DXVECTOR3 (0.0f, 1.0f, 0.0f));    // the up direction
+
+	//d3->SetTransform(D3DTS_VIEW, &matView);    // set the view transform to matView
+
+	//D3DXMATRIX matProjection;     // the projection transform matrix
+
+	//D3DXMatrixPerspectiveFovLH(&matProjection,
+	//						   D3DXToRadian(45),    // the horizontal field of view
+	//						   (FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT, // aspect ratio
+	//						   1.0f,    // the near view-plane
+	//						   50000.0f);    // the far view-plane
+
+	//d3->SetTransform(D3DTS_PROJECTION, &matProjection);    // set the projection
+	//d3->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
+	//d3->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+
+	//// renderinti scenos objektus
+	//d3->SetRenderState( D3DRS_LIGHTING, FALSE );
+	//d3->SetRenderState(D3DRS_FOGENABLE, FALSE);
+	//meshManager->drawMesh(d3, "skybox", kamera->getCamP().x, kamera->getCamP().y, kamera->getCamP().z, 50000, 0, 0, 0 );
+	//d3->SetRenderState(D3DRS_FOGENABLE, TRUE);
+	//d3->SetRenderState( D3DRS_LIGHTING, TRUE );
+	//
+	//if(wireframe)
+	//	d3->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+	//else
+	//	d3->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+
+	//
+	//
+	//	// RenderStates
+	//D3DRS_SLOPESCALEDEPTHBIAS, // Defaults to zero
+	//D3DRS_DEPTHBIAS,           // Defaults to zero
+
+
+	//stringstream ss1;//create a stringstream
+	//ss1 << "Slope: " << slopeScale << " Biass " << depthBias;
+	//
+
+	//gServerConsole.addLine(ss1.str(),GAME_CONSOLE_WARNING);
+	
+	//Vector camSpin;
+	//camSpin = kamera->getCamSpin();
+	//ter->Render(camSpin.x, camSpin.y, camSpin.z);
+
+	//d3->SetRenderState(D3DRS_SLOPESCALEDEPTHBIAS, (DWORD)slopeScale);
+	//d3->SetRenderState(D3DRS_DEPTHBIAS, (DWORD)depthBias);
+
+	
+
+	//CM->Render();
+	//sceneObjects->Render();
+
+	//// render hud
+	//hud->Render();
+	gServerConsole.render(d3);
+
+	//debesis->Render();
+
+	d3->EndScene();
+	d3->Present(NULL, NULL, NULL, NULL);
+}
+
+void SceneManager::Update(float dt)
+{
+	CM->Update(dt);
+	kamera->Update();
+
+	//ter->Update();
+	//Vector &vv = kamera->getCamP();
+	//sceneObjects->Update(vv.x, vv.y, vv.z);
+	//sceneObjects->Update(0, 0, 0);
+}
+
+
+void SceneManager::LoadObjects()
+{
+	meshManager->loadMesh("skybox/","skybox.x","skybox",d3);
+	meshManager->loadMesh("media/","kubas.x","kubas",d3);
+	
+	meshManager->loadMesh("Models/Aircraft/cesna/","cesna.x", "cesna", d3);
+	meshManager->loadMesh("Models/Aircraft/B-17/","B-17.x", "B17", d3);
+	meshManager->loadMesh("Models/","bomb.x", "bomba", d3);
+	meshManager->loadMesh("Models/","sfera.x", "sfera", d3);
+	meshManager->loadMesh("Models/","cannon.x", "cannon", d3);
+	meshManager->loadMesh("Models/","cannonBarrel.x", "cannonBarrel", d3);
+
+
+	meshManager->loadMesh("Models/Tree/tree1/","t1.x","t1",d3);
+	meshManager->loadMesh("Models/Tree/tree2/","t2.x","t2",d3);
+	meshManager->loadMesh("Models/Tree/tree3/","t3.x","t3",d3);
+	meshManager->loadMesh("Models/Tree/tree4/","t4.x","t4",d3);
+	meshManager->loadMesh("Models/Tree/tree5/","t5.x","t5",d3);
+	meshManager->loadMesh("Models/Tree/tree6/","t6.x","t6",d3);
+	meshManager->loadMesh("Models/Tree/tree7/","t7.x","t7",d3);
+
+	meshManager->loadMesh("Models/Tree/eglute/","eglute.x","eglute",d3);
+
+	meshManager->loadMesh("Models/Buildings/Fishing_house/","fish_house.x","fish_house",d3);
+	meshManager->loadMesh("Models/Buildings/Pilis/","pilis.x","pilis",d3);
+	meshManager->loadMesh("Models/Buildings/Gas_station/","gas_station.x","gas_station",d3);
+	meshManager->loadMesh("Models/Buildings/","lusna.x","lusna",d3);
+	meshManager->loadMesh("Models/Buildings/","lusna2.x","lusna2",d3);
+	meshManager->loadMesh("Models/Buildings/Mansion/","mansion.x","mansion",d3);
+	meshManager->loadMesh("Models/Buildings/","namas01.x","namas01",d3);
+	meshManager->loadMesh("Models/Buildings/","namas1.x","namas1",d3);
+	meshManager->loadMesh("Models/Buildings/","namas2.x","namas2",d3);
+	meshManager->loadMesh("Models/Buildings/","namas5.x","namas5",d3);
+	meshManager->loadMesh("Models/Buildings/","namas6.x","namas6",d3);
+	meshManager->loadMesh("Models/Buildings/","namas7.x","namas7",d3);
+	meshManager->loadMesh("Models/Buildings/","namas8.x","namas8",d3);
+	meshManager->loadMesh("Models/Buildings/","namas9.x","namas9",d3);
+	meshManager->loadMesh("Models/Buildings/","store.x","store",d3);
+	meshManager->loadMesh("Models/Buildings/","tavern.x","tavern",d3);
+
+}
+
+objektas *SceneManager::addObject(string ID, Vector position, Vector rotation, float scale)
+{
+	objektas *oo = NULL;
+	oo = sceneObjects->add(ID);
+
+	oo->pozicija.x = position.x;
+	oo->pozicija.y = position.y;
+	oo->pozicija.z = position.z;
+
+	oo->scale(scale, scale, scale);
+
+	oo->rotateYPR(rotation.x, rotation.y, rotation.z);
+
+	return oo;
+}
+
+objektas *SceneManager::addObject(string ID)
+{
+	return sceneObjects->add(ID);
+}
+
+
+void SceneManager::removeObject(objektas *oo)
+{
+	sceneObjects->remove(oo);
+}
+
+Kamera *SceneManager::getCamera()
+{
+	return kamera;
+}
+
+
+//Terrain *SceneManager::getTerrain()
+//{
+//	return ter;
+//}
+
+Mesh *SceneManager::getMeshManager()
+{
+	return meshManager;
+}
+
+Hud *SceneManager::getHud()
+{
+	return hud;
+}
+
+void SceneManager::changeRenderMode()
+{
+	if(wireframe)
+		wireframe = false;
+	else
+		wireframe = true;
+}
+
+
+/*
+void SceneManager::onOffConsolerender()
+{
+	if(renderConsole)
+		renderConsole = false;
+	else
+		renderConsole = true;
+}*/
+
+void SceneManager::slope(float delta)
+{
+	slopeScale += delta;
+}
+
+void SceneManager::bias(float delta)
+{
+	depthBias += delta;
+}
+
+ChunkManager *SceneManager::getChunkManager()
+{
+	return CM;
+}
+
+/*
+terrain *SceneManager::getTerrainAtPoint(D3DXVECTOR3 pos)
+{
+	return ter;
+}
+
+double SceneManager::get*/
